336da871a22404eb6aa4080a58675e564ed27624
NGUYENDANHNGHIA79 <nghiadanh09031993@gmail.com>
2026-05-14 11:40:56 +0700

    hello

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 // 📱 Test trên Expo Go: đổi thành IP LAN của máy tính (chạy ipconfig để xem)
 // 💻 Test trên web browser: đổi lại thành 'http://localhost:5000/api'
 // const BASE_URL = 'http://192.168.1.100:5000/api';
-const BASE_URL = 'http://172.20.10.2:5000/api';
+const BASE_URL = 'http://172.20.10.4:5000/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
